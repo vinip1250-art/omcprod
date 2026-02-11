e9ef8f3c2e60fae89e8bce15db27fdc696cdaba2
vinip1250-art <vinip1250@gmail.com>
2026-02-11 08:25:48 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,14 @@
+import "./globals.css"
+import { ReactNode } from "react"
 
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
